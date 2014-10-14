14e3dc45c5b88de80072ea711d7a4915d57cbdc0
Patsura Dmitry <zaets28rus@gmail.com>
2014-10-14 21:41:19 +1000

    Fix notFound

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,13 +1,14 @@
 <?php
 
-$router = new \Phalcon\Mvc\Router(true);
+$router = new \Phalcon\Mvc\Router(false);
 $router->removeExtraSlashes(true);
 $router->setUriSource(\Phalcon\Mvc\Router::URI_SOURCE_SERVER_REQUEST_URI);
 $router->setDefaultNamespace('Controllers');
+
+$router->notFound(array('controller' => 'index', 'action' => 'route404'));
 
 $router->add('/{action}', ['controller' => 'index'])->setName('action');
 $router->add('/{controller}/{action}')->setName('controller/action');
 $router->add('/{id}-{title}', ['controller' => 'index', 'action' => 'view', 'id' => 0, 'title' => 1])->setName('view/item');
 
-//$router->notFound(array('controller' => 'index', 'action' => 'route404'));
 return $router;
