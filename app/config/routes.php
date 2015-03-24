49753ad4d885908eec5b0f628166d288ef0382b5
Serebro <sergilyin@gmail.com>
2015-03-24 22:10:25 +0200

    fix

--- app/config/routes.php
+++ app/config/routes.php
@@ -10,7 +10,7 @@ $router->notFound(array('controller' => 'index', 'action' => 'route404'));
 $router->add('/{owner}', ['controller' => 'index', 'action' => 'viewOwner', 'owner' => 0])->setName('owner');
 $router->add('/{owner}/{repo}', ['controller' => 'index', 'action' => 'view', 'owner' => 0, 'repo' => 1])->setName('view/item');
 $router->add('/{controller:user|oauth}/{action}')->setName('controller/action');
-$router->add('/{id}-{title}', ['controller' => 'index', 'action' => 'view301', 'id' => 0, 'title' => 1]);
+$router->add('/{id:[0-9]+}-{title}', ['controller' => 'index', 'action' => 'view301', 'id' => 0, 'title' => 1]);
 $router->add('/owner/{owner}', ['controller' => 'index', 'action' => 'viewOwner301', 'owner' => 0]);
 $router->add('/{owner}/{repo}/{type}.svg', ['controller' => 'index', 'action' => 'badge', 'owner' => 0, 'repo' => 1, 'type' => 2])->setName('badge');
 $router->add('/category/{name}', ['controller' => 'index', 'action' => 'viewCategory', 'name' => 0])->setName('category');
